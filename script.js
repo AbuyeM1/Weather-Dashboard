3897249dea5b24a97398424e30ea98a5f4c4fc22
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-07 15:33:15 -0800

    adding code on script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// variables
+var searchButton = $(".searchButton");
+
+var apiKey = "bc0a3b94be4fd3f7064fb436fc2e3222";
+
+// forloop the persisting the the date onto HTML page
+for (var i = 0; i < localStorage; i++);
+
+var cityEl = localStorage.getItem(i);
+
+var cityNameEl = $(".list-group").add
