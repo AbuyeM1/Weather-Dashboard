c5846898c5977ad58e754f05bb792fbec7fe0630
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-07 23:03:23 -0800

    updating and adding some code

--- script.js
+++ script.js
@@ -1,11 +1,71 @@
-// variables
-var searchButton = $(".searchButton");
+// search button handeler
+$("#searchButton").on("click", searchWeather);
+var searchInput = $("#searchInput");
+var APIKEY="bc0a3b94be4fd3f7064fb436fc2e3222";
+function searchWeather() {
 
-var apiKey = "bc0a3b94be4fd3f7064fb436fc2e3222";
+    console.log(searchInput.val());
+    // add to history
+    addHistory(searchInput.val());
+    var cityName=searchInput.val();
+    var weatherAPIurl="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+APIKEY;
+    
+    $.ajax({
+        url:weatherAPIurl,
+        method:"GET",
+    }).then(function(response){
+        console.log(response);
 
-// forloop the persisting the the date onto HTML page
-for (var i = 0; i < localStorage; i++);
+        var date = new Date(response.dt * 1000).toLocaleDateString();
+        
+        var icon2=response.weather[0].icon;
+        var imageUrl2="http://openweathermap.org/img/wn/"+icon2+".png";
+        $("#cityName").html(response.name +"  ("+date+")"+"  <img src="+imageUrl2+">");
+        var farTemp1=((response.main.temp-273.15)*1.8)+32;
+        $("#temperature").html(farTemp1.toFixed(2));
+        $("#humidity").html(response.main.humidity+"%");
+        $("#windspeed").html(response.wind.speed);
 
-var cityEl = localStorage.getItem(i);
+        var lat=response.coord.lat;
+        var lon=response.coord.lon;
+        findUVIndex(lat,lon);
+    })
 
-var cityNameEl = $(".list-group").add
+    var forecastAPIurl="https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+APIKEY;
+
+    $.ajax({
+        url:forecastAPIurl,
+        method:"GET",
+    }).then(function(response){
+        console.log("Forecast",response);
+        
+        for(var i=0;i<5;i++){
+          var date2= new Date(response.list[((i+1)*8)-1].dt * 1000).toLocaleDateString();
+          
+          $("#day"+(i+1)).html(date2);
+          var farTemp2=((response.list[((i+1)*8)-1].main.temp-273.15)*1.8)+32;
+          $("#temp"+(i+1)).html(farTemp2.toFixed(2));
+          $("#humidity"+(i+1)).html(response.list[((i+1)*8)-1].main.humidity+"%");
+          var icon=response.list[((i+1)*8)-1].weather[0].icon;
+          var imageUrl="http://openweathermap.org/img/wn/"+icon+".png";
+          
+          $("#image"+(i+1)).html("<img src="+imageUrl+">")
+        }
+
+    })  
+ 
+}
+
+function findUVIndex(lat,lon){
+    var uvIndexUrl="http://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid="+APIKEY;
+    $.ajax({
+        url:uvIndexUrl,
+        method:"GET",
+    }).then(function(response){
+     $("#uvindex").html(response.value);
+    })
+}
+
+function addHistory(search){
+    //add to local storage
+}
